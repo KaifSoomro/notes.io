ed3d863f7966dc1c3651f0604a6ff1f28210032d
KaifSoomro <kaifsoomro82@gmail.com>
2025-07-19 02:03:58 +0500

    update

--- src/components/NoteForm.jsx
+++ src/components/NoteForm.jsx
@@ -35,7 +35,7 @@ const NoteForm = ({ isOpen, onClose, onSubmit, loading }) => {
 
   return (
     <div className='w-full h-screen bg-neutral-900/80 fixed top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] z-30 flex items-center justify-center px-3 md:px-0'>
-      <form onSubmit={handleSubmit} className='w-[280px] md:w-[550px] h-[330px] md:h-[370px] bg-amber-100 rounded-md pt-5 relative text-neutral-950'>
+      <form onSubmit={handleSubmit} className='w-full md:w-[550px] h-[330px] md:h-[370px] bg-amber-100 rounded-md pt-5 relative text-neutral-950'>
         <button type="button" onClick={onClose} className='absolute top-5 right-5 text-3xl cursor-pointer text-neutral-900'>
           <IoClose />
         </button>
@@ -61,7 +61,7 @@ const NoteForm = ({ isOpen, onClose, onSubmit, loading }) => {
           ) : (
             <button
               type='submit'
-              className='px-4 md:px-5 py-2 me-15 bg-neutral-800 text-white rounded ms-7 cursor-pointer'
+              className='px-4 md:px-5 py-2 me-9 md:me-15 bg-neutral-800 text-white rounded ms-7 cursor-pointer'
               disabled={loading}
             >
               Add
@@ -69,11 +69,11 @@ const NoteForm = ({ isOpen, onClose, onSubmit, loading }) => {
           )}
 
           <div className='w-full h-10 flex gap-3 md:gap-5 justify-end pe-7'>
-            <div onClick={() => setColor("bg-teal-500")} className='w-10 h-10 bg-teal-500 rounded-full cursor-pointer'></div>
-            <div onClick={() => setColor("bg-yellow-500")} className='w-10 h-10 bg-yellow-500 rounded-full cursor-pointer'></div>
-            <div onClick={() => setColor("bg-pink-500")} className='w-10 h-10 bg-pink-500 rounded-full cursor-pointer'></div>
-            <div onClick={() => setColor("bg-neutral-500")} className='w-10 h-10 bg-neutral-500 rounded-full cursor-pointer'></div>
-            <div onClick={() => setColor("bg-lime-500")} className='w-10 h-10 bg-lime-500 rounded-full cursor-pointer'></div>
+            <div onClick={() => setColor("bg-teal-500")} className='w-8 md:w-10 h-8 md:h-10 bg-teal-500 rounded-full cursor-pointer'></div>
+            <div onClick={() => setColor("bg-yellow-500")} className='w-8 md:w-10 h-8 md:h-10 bg-yellow-500 rounded-full cursor-pointer'></div>
+            <div onClick={() => setColor("bg-pink-500")} className='w-8 md:w-10 h-8 md:h-10 bg-pink-500 rounded-full cursor-pointer'></div>
+            <div onClick={() => setColor("bg-neutral-500")} className='w-8 md:w-10 h-8 md:h-10 bg-neutral-500 rounded-full cursor-pointer'></div>
+            <div onClick={() => setColor("bg-lime-500")} className='w-8 md:w-10 h-8 md:h-10 bg-lime-500 rounded-full cursor-pointer'></div>
           </div>
         </div>
       </form>
